24a0d1f75ae1ad73f70df449e1dbb1664011f143
Abraham Ochelle <abrahamochelleoga@gmail.com>
2024-09-25 22:24:17 -0500

    inital stuff

---
+++ blog.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'AB',
+        'Title' : 'First',
+        'content' : 'first post content',
+        'date_posted' : 'April 21, 2018'
+    }, 
+    {
+        'author': 'Simieyaa',
+        'Title' : 'Second',
+        'content' : 'second post content',
+        'date_posted' : 'April 21, 2018'
+    }
+]
+
+@app.route("/")
+
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
+    
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+if __name__=='__main__':
+    app.run(debug=True)
